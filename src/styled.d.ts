9de2db3e39e046cf423a5aecad2bed658d931d11
yuhwan-park <apkdkvkz@gmail.com>
2022-05-14 17:57:45 +0900

    material design 을 채용한 Default type scale 설정

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -1,5 +1,17 @@
 import 'styled-components';
 
 declare module 'styled-components' {
-  export interface DefaultTheme {}
+  export interface DefaultTheme {
+    fontSize: {
+      large: string;
+      medium: string;
+      small: string;
+    };
+    letterSpacing: {
+      body1: string;
+      body2: string;
+      caption: string;
+      button: string;
+    };
+  }
 }
--- src/theme.ts
+++ src/theme.ts
@@ -1,3 +1,15 @@
 import { DefaultTheme } from 'styled-components';
 
-export const Theme: DefaultTheme = {};
+export const Theme: DefaultTheme = {
+  fontSize: {
+    large: '1rem',
+    medium: '.875rem',
+    small: '.75rem',
+  },
+  letterSpacing: {
+    body1: '.5rem',
+    body2: '.25rem',
+    caption: '.4rem',
+    button: '1.25rem',
+  },
+};
