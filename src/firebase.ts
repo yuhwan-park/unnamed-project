db01614eda2c6f4497776fe8e7004ed5a0baaef1
yuhwan-park <apkdkvkz@gmail.com>
2022-05-16 12:00:20 +0900

    Figma UI Design

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -13,4 +14,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
 auth.useDeviceLanguage(); // 비밀번호 재설정 시 디바이스에 맞는 언어로 보내기 위한 함수
