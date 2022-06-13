186c900c4d30fef4386ef728ecd83376c7dd597a
yuhwan-park <apkdkvkz@gmail.com>
2022-06-14 06:37:16 +0900

    Feat: 캘린더 할일 개수 표시기능 추가

--- src/components/common/CalendarView.tsx
+++ src/components/common/CalendarView.tsx
@@ -1,6 +1,9 @@
+import { documentCountByDateState } from 'atoms';
 import dayjs from 'dayjs';
 import Calendar from 'react-calendar';
+import { useRecoilValue } from 'recoil';
 import 'style/calendar.css';
+import styled from 'styled-components';
 
 interface ICalendarViewProps {
   value: Date | null;
@@ -8,6 +11,7 @@ interface ICalendarViewProps {
 }
 
 function CalendarView({ value, onClickDay }: ICalendarViewProps) {
+  const docCount = useRecoilValue(documentCountByDateState);
   return (
     <Calendar
       formatDay={(locale, date) => dayjs(date).format('D')}
@@ -17,8 +21,21 @@ function CalendarView({ value, onClickDay }: ICalendarViewProps) {
       calendarType="US"
       showNeighboringMonth={false}
       onClickDay={onClickDay}
+      tileContent={({ date, view }) => {
+        const dateString = dayjs(date).format('YYYYMMDD');
+        if (docCount[dateString]) {
+          return <Count>{`(${docCount[dateString]})`}</Count>;
+        }
+        return null;
+      }}
     />
   );
 }
 
 export default CalendarView;
+
+const Count = styled.div`
+  position: absolute;
+  margin-left: 8px;
+  font-size: 8px;
+`;
