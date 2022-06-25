30999f4e6ba1f3ce00a683bd4f8d83eccd4ff979
yuhwan-park <apkdkvkz@gmail.com>
2022-06-25 21:54:06 +0900

    Test: Home.tsx 테스트 작성

--- src/__test__/home.test.tsx
+++ src/__test__/home.test.tsx
@@ -6,12 +6,34 @@ describe('홈페이지 버튼 테스트', () => {
   test('로그인 버튼을 클릭하면 Signin 페이지로 이동한다', async () => {
     const { user } = render();
 
-    const loginButton = screen.getByRole('link', { name: '로그인' });
+    const link = screen.getByRole('link', { name: '로그인' });
 
-    expect(loginButton).toBeInTheDocument();
-    await user.click(loginButton);
+    expect(link).toBeInTheDocument();
+    await user.click(link);
 
     // signin 페이지에만 있는 Text
     expect(screen.getByText(/계정이 없으신가요/i)).toBeInTheDocument();
+  });
+  test('회원가입 버튼을 클릭하면 Signup 페이지로 이동한다', async () => {
+    const { user } = render();
+
+    const link = screen.getByRole('link', { name: '회원가입' });
+
+    expect(link).toBeInTheDocument();
+    await user.click(link);
+
+    // signup 페이지에만 있는 Text
+    expect(screen.getByText(/이미 계정이 있으신가요/i)).toBeInTheDocument();
+  });
+  test('시작하기 버튼을 클릭하면 Signup 페이지로 이동한다', async () => {
+    const { user } = render();
+
+    const link = screen.getByRole('link', { name: '시작하기' });
+
+    expect(link).toBeInTheDocument();
+    await user.click(link);
+
+    // signup 페이지에만 있는 Text
+    expect(screen.getByText(/이미 계정이 있으신가요/i)).toBeInTheDocument();
   });
 });
