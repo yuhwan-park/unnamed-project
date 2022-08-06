2ee5955920ab3d3cc8eada7548c6e45422d62f59
yuhwan-park <apkdkvkz@gmail.com>
2022-08-06 20:37:41 +0900

    Refactor: GlobalStyle을 모듈화
    index.tsx에 있던 글로벌스타일을 모듈화하였음

--- src/index.tsx
+++ src/index.tsx
@@ -1,78 +1,15 @@
 import ReactDOM from 'react-dom/client';
 import App from 'App';
-import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 import { Theme } from 'theme';
 import { RecoilRoot } from 'recoil';
 import 'react-reflex/styles.css';
 import 'assets/fonts.css';
-import { devices } from 'style/media-queries';
+import { GlobalStyle } from 'style/GlobalStyle';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
-const GlobalStyle = createGlobalStyle`
-html, body, div, span, applet, object, iframe,
-h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-a, abbr, acronym, address, big, cite, code,
-del, dfn, em, img, ins, kbd, q, s, samp,
-small, strike, strong, sub, sup, tt, var,
-b, u, i, center,
-dl, dt, dd, menu, ol, ul, li,
-fieldset, form, label, legend,
-table, caption, tbody, tfoot, thead, tr, th, td,
-article, aside, canvas, details, embed,
-figure, figcaption, footer, header, hgroup,
-main, menu, nav, output, ruby, section, summary,
-time, mark, audio, video {
-  margin: 0;
-  padding: 0;
-  border: 0;
-  font-size: 100%;
-  font-family: ${props => props.theme.fontFamily.main};
-  vertical-align: baseline;
-}
-/* HTML5 display-role reset for older browsers */
-article, aside, details, figcaption, figure,
-footer, header, hgroup, main, menu, nav, section {
-  display: block;
-}
-/* HTML5 hidden-attribute fix for newer browsers */
-*[hidden] {
-    display: none;
-}
-body {
-  line-height: 1;
-  overflow: hidden;
-}
-menu, ol, ul {
-  list-style: none;
-}
-blockquote, q {
-  quotes: none;
-}
-blockquote:before, blockquote:after,
-q:before, q:after {
-  content: '';
-  content: none;
-}
-table {
-  border-collapse: collapse;
-  border-spacing: 0;
-}
-* {
-  box-sizing: border-box;
-  font-size: 16px;
-  font-family: 'Nanum Gothic', sans-serif;
-}
-a {
-  text-decoration: none;
-}
-.toggle-menu-icon {
-  @media ${devices.mobileL} {
-    opacity: 1 !important;
-  }
-}
-`;
 
 root.render(
   <RecoilRoot>
