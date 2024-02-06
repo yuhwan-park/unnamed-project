4b2d0192b505d5a01e41758eef80e50a87412e45
박유환 <apkdkvkz@gmail.com>
2024-02-06 16:21:12 +0900

    test commit 2

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -23,7 +23,7 @@ function Home() {
   return (
     <S.Wrapper>
       <S.Nav>
-        <S.Logo>dail-test</S.Logo>
+        <S.Logo>dail-test1</S.Logo>
         <S.Links>
           <Link to={'/signin'}>로그인</Link>
           <Link to={'/signup'}>회원가입</Link>
